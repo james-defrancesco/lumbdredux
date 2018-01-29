123262684e759b09d4324d39bea03dffe849c043
James De Francesco <jamesd@jamesd.fios-router.home>
2018-01-28 23:04:21 -0500

    added thunks and API calls for actions

--- src/App.js
+++ src/App.js
@@ -9,18 +9,23 @@ import {
 import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './rootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import Toggle from './Toggle';
+import Toggle from './Toggle'; 
+import logger from 'redux-logger';
+//return a function within our actions (allows to hit api)
+import thunk from 'redux-thunk';
 
 import MoviesList from './MoviesList';
 import MovieDetail from './MovieDetail';
 
+const middleware = [logger, thunk];
+
 const store = createStore(
   rootReducer,
   {},
-  composeWithDevTools(),
+  composeWithDevTools(applyMiddleware(...middleware)),
 );
 
 
--- src/Toggle.js
+++ src/Toggle.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 //importing our action
-import { toggleMessage } from './actions';
+import { toggleMessage, getMovies } from './actions';
 
 import { bindActionCreators } from 'redux';
 
 //stateless functional component
-const Toggle = ({ messageVisibility, toggleMessage }) => (
+const Toggle = ({ messageVisibility, toggleMessage, getMovies }) => (
     <div>
       {messageVisibility &&
         <p>You will see this if redux is toggled</p>
       }
       {/* dispatch is a function that accepts an object. in this case, an action. */}
         <button onClick={toggleMessage}>Toggle Me</button>
+        <button onClick={getMovies}>Load Movies</button>
     </div>
   );
 
@@ -23,6 +24,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => bindActionCreators({
   toggleMessage,
+  getMovies,
 }, dispatch);
 
 //connecting Toggle to redux store and exporting it
